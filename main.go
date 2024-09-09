fad6de2538d21a38364a4d69bbbd4d3e5435e83a
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-09-09 20:34:29 +0530

    starting to convert this module into a remote git repo

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/plantoncloud/gcs-bucket-pulumi-module/pkg"
+	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/code2cloud/v1/gcp/gcsbucket"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"google.golang.org/protobuf/encoding/protojson"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		stackInputString, ok := ctx.GetConfig("planton-cloud:stack-input")
+		if !ok {
+			return errors.New("stack-input not found in pulumi config")
+		}
+		stackInput := &gcsbucket.GcsBucket{}
+		if err := protojson.Unmarshal([]byte(stackInputString), stackInput); err != nil {
+			return fmt.Errorf("failed to unmarshal stack input: %v", err)
+		}
+		s := &pkg.ResourceStack{
+			StackInput: nil,
+		}
+		return s.Resources(ctx)
+	})
+}
